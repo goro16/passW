22e482d17aeaf35e7349cf4096ed69eeddc28b50
goro16 <80624782+goro16@users.noreply.github.com>
2022-03-29 21:53:21 +0200

    Update password.py

--- bin/password.py
+++ bin/password.py
@@ -4,12 +4,14 @@ import os
 
 def main():
 	os.system('clear')
-	print(" _ __   __ _ ___ ___  ")
-	print("| '_ \ / _` / __/ __| ")
-	print("| |_) | (_| \__ \__ \ ")
-	print("| .__/ \__,_|___/___/ ")
-	print("| |                   ")
-	print("|_|                   ")
+	print("                   __          __")
+	print("                   \ \        / /")
+	print("  _ __   __ _ ___ __\ \  /\  / / ")
+	print(" | '_ \ / _` / __/ __\ \/  \/ /  ")
+	print(" | |_) | (_| \__ \__ \\  /\  /   ")
+	print(" | .__/ \__,_|___/___/ \/  \/    ")
+	print(" | |                             ")
+	print(" |_|                             ")
 	print("created by goro16")
 	a = input("\n1- look the saves passwords\n2- add a new password\n3- delete a password\n4- exit\nchoose a number : ")
 
@@ -34,19 +36,19 @@ def passw():
 	os.system('clear')
 	print("this is your passwords : ")
 	os.system('cat db.txt')
-	b = input("\n1- return to menu\n2- find a password\n3- exit\nchoose a number : ")
+	a = input("\n1- return to menu\n2- find a password\n3- exit\nchoose a number : ")
 
 
-	if b == '1':
+	if a == '1':
 		os.system('clear')
 		main()
 
-	if b == '2':
+	if a == '2':
 		os.system('clear')
 		search()
 
 
-	if b == '3':
+	if a == '3':
 		os.system('clear')
 		exit()
 
@@ -58,15 +60,15 @@ def add():
 	user = input('what is the username : ')
 	password = input('what is the password : ')
 	os.system(f'echo "{url}, {user}, {password}" >> db.txt')
-	c = input("\n1- return to menu\n2- exit\nchoose a number : ")
+	a = input("\n1- return to menu\n2- exit\nchoose a number : ")
 
 
-	if c == '1':
+	if a == '1':
 		os.system('clear')
 		main()
 
 
-	if c == '2':
+	if a == '2':
 		os.system('clear')
 		exit()
 
@@ -81,15 +83,15 @@ def delt():
 	os.system(f"sed '{ligne}d' db.txt >> db2.txt")
 	os.system('rm -rf db.txt')
 	os.system('mv db2.txt db.txt')
-	d = input("\n1- return to menu\n2- exit\nchoose a number : ")
+	a = input("\n1- return to menu\n2- exit\nchoose a number : ")
 
 
-	if d == '1':
+	if a == '1':
 		os.system('clear')
 		main()
 
 
-	if d == '2':
+	if a == '2':
 		os.system('clear')
 		exit()
 
@@ -98,19 +100,19 @@ def search():
 	os.system('clear')
 	print('here is the result of your search : ')
 	os.system(f'cat db.txt | grep {a}')
-	d = input("\n1- return to menu\n2- find another password\n3- exit\nchoose a number : ")
+	b = input("\n1- return to menu\n2- find another password\n3- exit\nchoose a number : ")
 
 
-	if d == '1':
+	if b == '1':
 		os.system('clear')
 		main()
 
-	if d == '2':
+	if b == '2':
 		os.system('clear')
 		search()
 
 
-	if d == '3':
+	if b == '3':
 		os.system('clear')
 		exit()
 
